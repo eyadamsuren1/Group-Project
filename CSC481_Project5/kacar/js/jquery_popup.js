3c793acf0553aa2e37702339c8ffde0e13954f4d
eyadamsuren1 <32419590+eyadamsuren1@users.noreply.github.com>
2017-10-31 16:05:46 -0700

    Changes

--- CSC481_Project5/kacar/js/jquery_popup.js
+++ CSC481_Project5/kacar/js/jquery_popup.js
@@ -109,4 +109,33 @@ $(document).ready(function() {
 	    }, 'slow');
 	    return false;
 	});
+    //Loop through all Labels with class 'editable'.
+    $(".editable").each(function () {
+        //Reference the Label.
+        var label = $(this);
+        //Add a TextBox next to the Label.
+        label.after("<input type = 'text' style = 'display:none' />");
+        //Reference the TextBox.
+        var textbox = $(this).next();
+ 
+        //Set the name attribute of the TextBox.
+        var id = this.id.split('_')[this.id.split('_').length - 1];
+        textbox[0].name = id.replace("lbl", "txt");
+ 
+        //Assign the value of Label to TextBox.
+        textbox.val(label.html());
+ 
+        //When Label is clicked, hide Label and show TextBox.
+        label.click(function () {
+            $(this).hide();
+            $(this).next().show();
+        });
+ 
+        //When focus is lost from TextBox, hide TextBox and show Label.
+        textbox.focusout(function () {
+            $(this).hide();
+            $(this).prev().html($(this).val());
+            $(this).prev().show();
+        });
+    });
 });
