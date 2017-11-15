8c4218a22226869bf69e3e31c16cacae774da805
avilanova1 <avilanova1@toromail.csudh.edu>
2017-11-15 11:02:43 -0800

    Update

--- CSC481_Project5/kacar/PHP/loginKacar.php
+++ CSC481_Project5/kacar/PHP/loginKacar.php
@@ -1,15 +1,16 @@
 <?php  
 ///test/demo_form.php?name1=value1&name2=value2
 //echo 'hello';
-	if(isset($_POST["email"]) && isset($_POST['password']))
-	{
-		//echo $_POST['email'];
-		//echo $_POST['password'];
-		$servername = "localhost";
-		$username = "root";
-		$password = "root";
-		
-		class User{
+if(isset($_POST["email"]) && isset($_POST['password']))
+{
+	//echo $_POST['email'];
+	//echo $_POST['password'];
+	$servername = "localhost";
+	$username = "root";
+	$password = "HelloWorld5";
+	
+	
+	class User{
 			public $userid = "";
 			public $firstName = "";
 			Public $lastName = "";
@@ -18,80 +19,128 @@
 			public $renter_status = "";
 			public $profile_pic_dir="";
 			public $vehicles = array();
-		}
-		class Vehicle{
-			public $vehicleid = "";
-			public $model = "";
-			public $year = "";
-			public $vin = "";
-			public $miles = "";
-		}
+	}
+	class Vehicle{
+		public $vehicleid = "";
+		public $model = "";
+		public $year = "";
+		public $vin = "";
+		public $miles = "";
+	}
 		
-		$user = new User();
-		$vehicleList = array();
-		//$classroom = array();
 		
-		// Create connection
-		$conn = new mysqli($servername, $username, $password);
+	$user = new User();
+	$vehicleList = array();
+	$mysqli = new mysqli("localhost", "root", "root", "kacar");
 
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		} 
-		//echo "Connected successfully";
-		
-		//$sql = "SELECT * FROM Kacar.user;";
-		$sql = "SELECT * FROM Kacar.user INNER JOIN Kacar.Vehicles ON user.userid = Vehicles.ownerid AND user.email ='" . $_POST['email'] . "' AND user.password = '" . $_POST['password'] . "';";
-		//$sql = "SELECT * FROM Kacar.user INNER JOIN Kacar.Vehicles ON user.userid = Vehicles.ownerid;";
-		$result = $conn->query($sql);
-		//$user->classrooms = $result;
-		if($result->num_rows > 0) {
-			while($row = $result->fetch_assoc()) {
-				if($user->userid != $row['userid']) {
-					//echo $row['userid'];
-					$user->userid = $row['userid'];
-					$user->firstName = $row['fname'];
-					$user->lastName = $row['lname'];
-					$user->email = $row['email'];
-					$user->password = $row['password'];
-					$user->renter_status = $row['renter_status'];
-					$user->profile_pic_dir = $row['profile_pic_dir'];
+	/* check connection */
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+
+	//$query  = "SELECT * FROM class;";
+	//$query = "SELECT * FROM user";
+	
+	//$sql = "";
+	$sql = "SELECT * FROM Kacar.user INNER JOIN Kacar.Vehicles ON user.userid = Vehicles.ownerid AND user.email ='" . $_POST['email'] . "' AND user.password = '" . $_POST['password'] . "';";
+	$sql .= "SELECT * FROM Kacar.user WHERE user.email ='" . $_POST['email'] . "' AND user.password = '" . $_POST['password'] . "'";
+	
+	//echo $sql;
+	//$end = FALSE;
+
+	$sql_count = 0;
+	/* execute multi query */
+	if ($mysqli->multi_query($sql)) {
+		do {
+			$sql_count += 1;
+			/* store first result set */
+			if ($result = $mysqli->store_result()) {
+				  $rowcount=mysqli_num_rows($result);
+				 
+				if($sql_count == 1 && $rowcount != 0)
+				{
+					while ($row = $result->fetch_row()) {
+						
+						if($user->userid != $row[0])
+						{
+							$user->userid = $row[0];
+							$user->firstName = $row[1];
+							$user->lastName = $row[2];
+							$user->email = $row[3];
+							$user->password = $row[4];
+							$user->renter_status = $row[5];
+							$user->profile_pic_dir = $row[6];
+							//echo $row['fname'];
+							//$myJSON = json_encode($row);
+							//echo $myJSON;
+							//array_push($classroom, $row);
+							//echo $row["id"];
+							//echo $myJSON;
+							
+							$vehicles = new Vehicle();
+							$vehicles->vehicleid = $row[7];
+							$vehicles->model = $row[9];
+							$vehicles->year = $row[10];
+							$vehicles->vin = $row[11];
+							$vehicles->miles = $row[12];
+							array_push($vehicleList, $vehicles);
+						} else {
+							$vehicles = new Vehicle();
+							$vehicles->vehicleid = $row[7];
+							$vehicles->model = $row[9];
+							$vehicles->year = $row[10];
+							$vehicles->vin = $row[11];
+							$vehicles->miles = $row[12];
+							array_push($vehicleList, $vehicles);
+						}
+					}
+					break;
+				}
+				if ($sql_count == 2){
+					$row = $result->fetch_row();
+					$user->userid = $row[0];
+					$user->firstName = $row[1];
+					$user->lastName = $row[2];
+					$user->email = $row[3];
+					$user->password = $row[4];
+					$user->renter_status = $row[5];
+					$user->profile_pic_dir = $row[6];
 					//echo $row['fname'];
 					//$myJSON = json_encode($row);
 					//echo $myJSON;
 					//array_push($classroom, $row);
 					//echo $row["id"];
 					//echo $myJSON;
-					
-					$vehicles = new Vehicle();
-					$vehicles->vehicleid = $row['vehicleid'];
-					$vehicles->model = $row['model'];
-					$vehicles->year = $row['year'];
-					$vehicles->vin = $row['vin'];
-					$vehicles->miles = $row['miles'];
-					array_push($vehicleList, $vehicles);
-					
-				} else {
-					$vehicles = new Vehicle();
-					$vehicles->vehicleid = $row['vehicleid'];
-					$vehicles->model = $row['model'];
-					$vehicles->year = $row['year'];
-					$vehicles->vin = $row['vin'];
-					$vehicles->miles = $row['miles'];
-					array_push($vehicleList, $vehicles);
+					break;
 				}
+				
+				
+				
+				
+				$result->free();
 			}
-		}
-		
-		$user->vehicles = $vehicleList;
-		$myJSON = json_encode($user);
-		
-		if($user->userid == "") {
-			echo 'null';
-		} else {
-			echo $myJSON;
-		}
+			
+		} while ($mysqli->next_result());
+	}
+
+	$user->vehicles = $vehicleList;
+	$myJSON = json_encode($user);
+	
+	
+	/* close connection */
+	$mysqli->close();
+	if($user->userid == "") {
+		echo 'null';
 	} else {
+		echo $myJSON;
+	}
+
+	}
+	else
+	{
 		echo 'null';
 	}
+
+			
 ?>
