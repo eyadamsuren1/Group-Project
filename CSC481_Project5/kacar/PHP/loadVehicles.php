2c215720f5c001827809af5e45912ca0a990b9ec
avilanova1 <tonyvila94@yahoo.com>
2017-11-18 12:56:08 -0800

    load

---
+++ CSC481_Project5/kacar/PHP/loadVehicles.php
@@ -0,0 +1,135 @@
+<?php  
+///test/demo_form.php?name1=value1&name2=value2
+//echo 'hello';
+
+	//echo $_POST['email'];
+	//echo $_POST['password'];
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	
+
+	
+	class Vehicle{
+		public $vehicleid = "";
+		public $model = "";
+		public $year = "";
+		public $vin = "";
+		public $miles = "";
+		public $picdir = array();
+	}
+	
+	class PicDir{
+		
+		//public $vehicleid = "";
+		public $dir = array();
+	}
+		
+		
+	
+	$vehicleList = array();
+	$picdir = new PicDir();
+	$mysqli = new mysqli("localhost", "root", "root", "kacar");
+
+	/* check connection */
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+
+	//$query  = "SELECT * FROM class;";
+	//$query = "SELECT * FROM user";
+	
+	//$sql = "";
+	//$sql = "SELECT * FROM Vehicles LIMIT 0, 10;";
+	$sql = "SELECT * FROM Vehicles;";
+	$sql .= "SELECT * FROM pic_dir;";
+	
+	//echo $sql;
+	//$end = FALSE;
+
+	$sql_count = 0;
+	/* execute multi query */
+	if ($mysqli->multi_query($sql)) {
+		do {
+			$sql_count += 1;
+			/* store first result set */
+			if ($result = $mysqli->store_result()) {
+				  $rowcount=mysqli_num_rows($result);
+					//echo $rowcount;
+				if($sql_count == 1 && $rowcount != 0)
+				{
+					while ($row = $result->fetch_row()) {
+						
+						
+							$vehicle = new Vehicle();
+							$vehicle->vehicleid = $row[0];
+							$vehicle->model = $row[2];
+							$vehicle->year = $row[3];
+							$vehicle->vin = $row[4];
+							$vehicle->miles = $row[5];
+							//echo $row['fname'];
+							//$myJSON = json_encode($row);
+							//echo $myJSON;
+							//array_push($classroom, $row);
+							//echo $row["id"];
+							//echo $myJSON;
+							//$dir = new PicDir();
+							//$vehicle->picdir = $dir;
+							array_push($vehicleList, $vehicle);
+							
+							
+						
+					}
+					
+				    //echo $myJSON;
+					//break;
+				}
+				if ($sql_count == 2){
+					
+					$arrlength = count($vehicleList);
+					while($row = $result->fetch_row())
+					{
+						for($x = 0; $x < $arrlength; $x++)
+						{
+							$currentid = $vehicleList[$x]->vehicleid;
+
+							if($currentid == $row[0])
+							{
+								//echo "\n" .$currentid;
+								//echo $row[1];
+								array_push($vehicleList[$x]->picdir, $row[1]);
+
+								//$vehicleList[$x]->picdir
+							}
+
+						}
+					
+					}
+					$myJSON = json_encode($vehicleList);
+					echo $myJSON;
+					break;
+				}
+				
+				
+				
+				
+				$result->free();
+			}
+			
+		} while ($mysqli->next_result());
+	}
+
+	
+	
+	
+	/* close connection */
+	$mysqli->close();
+	//if($user->userid == "") {
+		//echo 'null';
+	//} else {
+		//echo $myJSON;
+	//}
+
+			
+?>
