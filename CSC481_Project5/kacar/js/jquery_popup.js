9fd0559049bece95622d801cfe7ce527fdb5afeb
eyadamsuren1 <32419590+eyadamsuren1@users.noreply.github.com>
2017-11-21 22:52:34 -0800

    My Google Map not Working

--- CSC481_Project5/kacar/js/jquery_popup.js
+++ CSC481_Project5/kacar/js/jquery_popup.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+
+    function myMap() {
+        var myCenter = new google.maps.LatLng(51.508742,-0.120850);
+        var mapCanvas = document.getElementById("map");
+        var mapOptions = {center: myCenter, zoom: 5};
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+        var marker = new google.maps.Marker({position:myCenter});
+        marker.setMap(map);
+
+        // Zoom to 9 when clicking on marker
+        google.maps.event.addListener(marker,'click',function() {
+            map.setZoom(9);
+            map.setCenter(marker.getPosition());
+        });
+    }
+
     $("#sign_up").click(function() {
         $("#signupdiv").css("display", "block");
     });
--- CSC481_Project5/kacar/js/photobox.js
+++ CSC481_Project5/kacar/js/photobox.js
@@ -1,15 +1,4 @@
 (function($) {
-	function initMap() {
-		var uluru = {lat: -25.363, lng: 131.044};
-		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 4,
-			center: uluru
-		});
-		var marker = new google.maps.Marker({
-		position: uluru,
-		map: map
-		});
-	}
 	function Photo_Box(opts) {
 		this.settings = $.extend({}, $.fn.PhotoBox.defaults, opts);
 		this.bodyDimension = {width:0,height:0};
@@ -335,40 +324,3 @@
 		imageClassName: 'photobox-target-img'
 	};
 }(jQuery));
-
-/*
-https://www.w3schools.com/graphics/tryit.asp?filename=trymap_events_click
-<!DOCTYPE html>
-<html>
-
-<body>
-
-<div id="map" style="width:100%;height:500px"></div>
-
-<script>
-function myMap() {
-  var myCenter = new google.maps.LatLng(51.508742,-0.120850);
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {center: myCenter, zoom: 5};
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-  var marker = new google.maps.Marker({position:myCenter});
-  marker.setMap(map);
-
-  // Zoom to 9 when clicking on marker
-  google.maps.event.addListener(marker,'click',function() {
-    map.setZoom(9);
-    map.setCenter(marker.getPosition());
-  });
-}
-</script>
-
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
-<!--
-To use this code on your website, get a free API key from Google.
-Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
--->
-
-</body>
-</html>
-
-*/
