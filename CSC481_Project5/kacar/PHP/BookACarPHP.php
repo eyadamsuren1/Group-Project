66c3bec87cd6e64418b59a96632f57fc6f7ae2a8
Nishita Parmar <32343461+nishita-999@users.noreply.github.com>
2017-12-16 00:45:33 -0800

    Update BookACarPHP.php

--- CSC481_Project5/kacar/PHP/BookACarPHP.php
+++ CSC481_Project5/kacar/PHP/BookACarPHP.php
@@ -1,6 +1,25 @@
 <?php
 if(isset($_POST['submit'])){
     	session_start();
+		
+        $data_missing = array();
+        if(empty($_POST['renterid'])){
+            // Adds name to array
+            $data_missing[] = 'Renter Id';
+
+        } else {
+            // Trim white space from the name and store the name
+            $renterid = trim($_POST['renterid']);
+        }
+		if(empty($_POST['vehicleid'])){
+            // Adds name to array
+
+            $data_missing[] = 'Vehicle Id';
+        } else {
+            // Trim white space from the name and store the name
+            $vehicleid = trim($_POST['vehicleid']);
+        }
+		if(empty($data_missing)){
 			$servername = "localhost";
     		$db_username = "root";
     		$db_password = "root";
@@ -12,15 +31,18 @@ if(isset($_POST['submit'])){
             }
             echo "Connected successfully";
 			            
-            $query1 = "SELECT * FROM kacar.booked_vehicle WHERE vehicleid ='" . $'vehicleid'."' ";
-			
+            $query1 = "SELECT * FROM kacar.booked_vehicles WHERE vehicleid ='" . $vehicleid ."' ";
 			$result = $dbc->query($query1);
+			
+//			echo "query 1";
 			if($result->num_rows > 0)
 			{
 				echo "Vehicle is already booked";
 			} else {
-				$query2 = "INSERT INTO kacar.booked_vehicle (vehicleid, renterid) VALUES ('".$vehicleid."','".$renterid."')";
+
+				$query2 = "INSERT INTO kacar.booked_vehicles (vehicleid, renterid) VALUES ('".$vehicleid."','".$renterid."')";
 				if($dbc->query($query2) === TRUE) {
+					
 						echo 'The vehicle is now booked';
 				}
 				else {
@@ -28,6 +50,14 @@ if(isset($_POST['submit'])){
 	                echo mysqli_error();
 				}
 			}
+		}
+		else {
+            echo 'You need to enter the following data inside <br />';
+            foreach($data_missing as $missing) {
+                echo "$missing<br />";
+            }
+        }
         mysqli_close($dbc);
-}
+	}
+
 ?>
